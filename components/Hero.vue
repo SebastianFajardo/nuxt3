cc60b8faecdec77a0642edae6da58df45c7a4e63
Sebastian Fajardo <sebastian.fajardo@savne.net>
2023-11-27 15:11:46 -0500

    First Commit

---
+++ components/Hero.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="mt-40 w-full lg:px-10">
+    <h1
+        class="font-medium mt-20 w-full text-4xl text-dark-900 lg:text7xl lg:w-3/4 dark-text-gray-50"
+    >
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis consectetur consequatur culpa cum cupiditate dignissimos ducimus incidunt, ipsa, iste itaque nesciunt perferendis praesentium quae quasi repellendus rerum, sequi sunt voluptas?
+    </h1>
+    <div class="py-40">
+      <Galery/>
+    </div>
+  </div>
+</template>
---
+++ components/SwithMode.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <button
+      v-if="$colorMode.preference === 'dark'"
+      class="flex h-auto w-auto items-center"
+      @click="toggleMode('light')"
+    >
+      <svg
+          class="h-8 fill-current text-gray-50 w-8"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+            fill-rule="evenodd"
+            d="M12 2a1 1 0 0 1 1 1v1a1 1 0 1 1-2 0V3a1 1 0 0 1 1-1zm7.071 2.929a1 1 0 0 1 0 1.414l-.707.707a1 1 0 1 1-1.414-1.414l.707-.707a1 1 0 0 1 1.414 0zm-14.142 0a1 1 0 0 1 1.414 0l.707.707A1 1 0 0 1 5.636 7.05l-.707-.707a1 1 0 0 1 0-1.414zM12 8a4 4 0 1 0 0 8 4 4 0 0 0 0-8zm-6 4a6 6 0 1 1 12 0 6 6 0 0 1-12 0zm-4 0a1 1 0 0 1 1-1h1a1 1 0 1 1 0 2H3a1 1 0 0 1-1-1zm17 0a1 1 0 0 1 1-1h1a1 1 0 1 1 0 2h-1a1 1 0 0 1-1-1zM5.636 16.95a1 1 0 0 1 1.414 1.414l-.707.707a1 1 0 0 1-1.414-1.414l.707-.707zm11.314 1.414a1 1 0 0 1 1.414-1.414l.707.707a1 1 0 0 1-1.414 1.414l-.707-.707zM12 19a1 1 0 0 1 1 1v1a1 1 0 1 1-2 0v-1a1 1 0 0 1 1-1z"
+            clip-rule="evenodd"
+        ></path>
+      </svg>
+    </button>
+
+    <button v-else @click="toggleMode('dark')">
+      <svg
+          class="h-8 fill-current text-dark-900 w-8"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+            fill-rule="evenodd"
+            d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+            clip-rule="evenodd"
+        ></path>
+      </svg>
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const colorMode= useColorMode();
+  function toggleMode(mode : string) {
+    colorMode.preference = mode
+  }
+</script>
